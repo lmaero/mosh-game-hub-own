a66e3d3751e6b6bbaae41ab9fda142e6395880da
Luis Guzman <luis.guzman@falcani.com>
2023-09-13 15:05:25 -0500

    Fetch the genres

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import GameGrid from '@/components/GameGrid'
+import GenreList from '@/components/GenreList'
 import { Navbar } from '@/components/Navbar'
 import { Grid, GridItem, Show } from '@chakra-ui/react'
 
@@ -17,7 +18,7 @@ export default function Home() {
       </GridItem>
 
       <Show above={'lg'}>
-        <GridItem area={'aside'}>Aside</GridItem>
+        <GridItem area={'aside'}>{<GenreList />}</GridItem>
       </Show>
 
       <GridItem area={'main'}>
